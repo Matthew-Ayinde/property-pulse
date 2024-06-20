f601479df6984c637ada574faeb663c54c14ef72
Matthew Ayinde <ayindematthew2003@gmail.com>
2024-06-20 18:14:41 +0100

    Created the whole front end

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/Footer'
+import '@/assets/styles/globals.css'
+
+export const metadata = {
+  title: 'Property Pulse | Find the perfect rental',
+  description: 'Find your dream rental property'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  )
+}
+
+export default MainLayout
---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '@/components/Hero'
+import InfoBoxes from '@/components/InfoBoxes'
+import HomeProperties from '@/components/HomeProperties'
+
+
+
+const page = () => {
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
+  )
+}
+
+export default page
